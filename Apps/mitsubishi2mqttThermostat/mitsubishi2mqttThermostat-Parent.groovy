c82e70a7c0bfff3d5807dc0ee75d426af635d559
dtherron <77552027+dtherron@users.noreply.github.com>
2021-01-30 14:09:46 -0800

    Update mitsubishi2mqttThermostat-Parent.groovy
    update metadata

--- Apps/mitsubishi2mqttThermostat/mitsubishi2mqttThermostat-Parent.groovy
+++ Apps/mitsubishi2mqttThermostat/mitsubishi2mqttThermostat-Parent.groovy
@@ -1,9 +1,9 @@
 /*
- *  Advanced vThermostat Parent App
- *  Project URL: https://github.com/NelsonClark/Hubitat/tree/main/Apps/Advanced_vThermostat
- *  Copyright 2020 Nelson Clark
+ *  Mitsubishi2Mqtt Thermostat Parent App
+ *  Project URL: https://github.com/dtherron/Hubitat/edit/main/Apps/mitsubishi2mqttThermostat
+ *  Copyright 2021 Dan Herron
  *
- *  This app requires it's child app and device driver to function, please go to the project page for more information.
+ *  This app requires its child app and device driver to function; please go to the project page for more information.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -17,23 +17,23 @@
  */
 
 definition(
-	name: "Advanced vThermostat Manager",
-	namespace: "nclark",
-	author: "Nelson Clark",
+	name: "Mitsubishi2Mqtt Thermostat Manager",
+	namespace: "dtherron",
+	author: "Dan Herron",
 	description: "Join any sensor(s) with any outlet(s) for virtual thermostat control.",
 	category: "Green Living",
-	iconUrl: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/Advanced_vThermostat/Advanced_vThermostat-logo-small.png",
-	iconX2Url: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/Advanced_vThermostat/Advanced_vThermostat-logo.png",
-	importUrl: "https://raw.githubusercontent.com/NelsonClark/Hubitat/main/Apps/Advanced_vThermostat/Advanced_vThermostat-Parent.groovy",
+	iconUrl: "https://raw.githubusercontent.com/dtherron/Hubitat/main/Apps/mitsubishi2mqttThermostat/mitsubishi2mqttThermostat-logo-small.png",
+	iconX2Url: "https://raw.githubusercontent.com/dtherron/Hubitat/main/Apps/mitsubishi2mqttThermostat/mitsubishi2mqttThermostat-logo.png",
+	importUrl: "https://raw.githubusercontent.com/dtherron/Hubitat/main/Apps/mitsubishi2mqttThermostat/mitsubishi2mqttThermostat-Parent.groovy",
 	singleInstance: true
 )
 
 preferences {
-	page(name: "Install", title: "Advanced vThermostat Manager", install: true, uninstall: true) {
+	page(name: "Install", title: "Mitsubishi2Mqtt Thermostat Manager", install: true, uninstall: true) {
 		section("Devices") {
 		}
 		section {
-			app(name: "thermostats", appName: "Advanced vThermostat Child", namespace: "nclark", title: "Add Advanced vThermostat", multiple: true)
+			app(name: "thermostats", appName: "Mitsubishi2Mqtt Thermostat Child", namespace: "dtherron", title: "Add Mitsubishi2Mqtt Thermostat", multiple: true)
 		}
 	}
 }
